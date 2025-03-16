8000390ac97b807146d1bab1ba71a1170b7fb5a1
Jackop05 <jakubnyrbotzs@gmail.com>
2025-03-16 22:25:39 +0100

    Commit

--- src/main/java/com/onkore_backend/onkore/Config/WebConfig.java
+++ src/main/java/com/onkore_backend/onkore/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://onkore.pl") // Allow only frontend
+                .allowedOrigins("https://onkore.pl", "https://www.onkore.pl") // Allow only frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // Must be true for cookies
