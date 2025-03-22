2a43d41671bdf52edb76a2b1d4e5a372fa9512a8
Jackop05 <jakubnyrbotzs@gmail.com>
2025-03-22 10:34:37 +0100

    Commit

--- src/main/java/com/onkore_backend/onkore/Config/WebConfig.java
+++ src/main/java/com/onkore_backend/onkore/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")  //"https://onkore.pl", "https://www.onkore.pl", "http://localhost:5173", "https://onkore-frontend-prototype.netlify.app/"
+                .allowedOrigins("*")  // //"https://onkore.pl", "https://www.onkore.pl", "http://localhost:5173", "https://onkore-frontend-prototype.netlify.app/"
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // Must be true for cookies
