30c6da502fea5281fe83cff3148e2cbadc70ef4e
Jackop05 <jakubnyrbotzs@gmail.com>
2025-03-22 10:38:03 +0100

    Commit

--- src/main/java/com/onkore_backend/onkore/Config/MongoConfig.java
+++ src/main/java/com/onkore_backend/onkore/Config/MongoConfig.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class MongoConfig {
 
     @Value("${spring.data.mongodb.uri}")
-    private String mongoUri;
+    private String mongoUri;  // mongo uri
 
     @Bean
     public MongoClient mongoClient() {
